50b548622777d872b3a470e6903a9bf55118c6a0
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-11-25 18:37:13 -0500

    Fix version that has “c” in it

--- lib/config.sh
+++ lib/config.sh
@@ -28,7 +28,8 @@
 #############################################################################
 
 # Default branch
-RELVER=$(/usr/bin/gawk '$1 == "OmniOS" {sub(/r/,""); print $3 }' /etc/release)
+# RELVER=$(/usr/bin/gawk '$1 == "OmniOS" {sub(/r/,""); print $3 }' /etc/release)
+RELVER=151024
 PVER=0.$RELVER
 
 # Which server to fetch files from

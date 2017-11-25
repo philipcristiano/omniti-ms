6fc54b3b5edced67f74b81d87bb3e06b980a21c9
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-11-25 18:11:58 -0500

    config: Set server

--- lib/site.sh
+++ lib/site.sh
@@ -1,11 +1,11 @@
 # Package server URL and publisher
 : ${PKGPUBLISHER=ms.omniti.com}
-: ${PKGSRVR=http://10.0.1.132:10000/}
+PKGSRVR=http://10.0.1.132:10000/
 
 # To create a on-disk repo in the top level of your checkout
 # and publish there instead of the URI specified above.
 #
-PKGSRVR=file:///$MYDIR/../tmp.repo/
+# PKGSRVR=file:///$MYDIR/../tmp.repo/
 
 export PATH=$PATH:/opt/omni/bin
 
